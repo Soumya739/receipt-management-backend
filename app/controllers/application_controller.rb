25b0f8c625b5e028a7331ff1b720640ff177a948
Soumya_Goyal <somyagoyal739@gmail.com>
2020-01-09 15:39:10 -0800

    added issue_token, token, decoded_token, user_id, current_user and logged_in? methods in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,33 @@
 class ApplicationController < ActionController::API
+    def issue_token(user)
+        # encode is inbuilt method in jwt - 1st argument= payload, 2nd - seed string, algorithem
+        # for more info look jwt website
+        JWT.encode({user_id: user.id}, 'soumya_secret_key', 'HS256')
+    end
+
+    def token
+        request.headers['Authorization']
+    end
+
+    def decoded_token
+        begin
+            JWT.decode(token, 'soumya_secret_key', true, { :algorithm => 'HS256' })
+        rescue JWT::DecodeError
+            [{error: "Invalid Token"}]
+        end
+    end
+
+    def user_id
+        decoded_token.first['user_id']
+    end 
+
+    def current_user
+        @user ||= User.find_by(id: user_id)
+    end
+
+    def logged_in?
+    !!current_user
+    end
+
+
 end
