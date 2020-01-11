ecde5da74ce2c4e1a050ac1e8d18606c23ff5824
Soumya_Goyal <somyagoyal739@gmail.com>
2020-01-10 21:16:17 -0800

    tmp

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::AuthController < ApplicationController
+class AuthController < ApplicationController
     def create
         user = User.find_by(email: params[:email])
         if user && user.authenticate(params[:password])
